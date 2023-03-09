fix update masking

experiments/train_ps.py
@@ -44,6 +44,10 @@ def main(seed=42, log_dir=None, data_dir=None,
          batch_size=128, num_workers=4,
          optimizer_type='sgd', lr=.1, alpha=0., momentum=.9, reg_scale=0., epochs=0,
          swa_epochs=0, swa_lr=0.05, swag_rank=0, swag_samples=0):
+    
+    ## SWA updates happen every
+    swag_rank = min(swa_epochs, swag_rank)
+    
     wandb.config.update({
         'log_dir': log_dir,
         'seed': seed,
@@ -81,7 +85,7 @@ def main(seed=42, log_dir=None, data_dir=None,
     if optimizer_type == 'sgd':
         optimizer = optax.chain(
             optax.add_decayed_weights(reg_scale),
-            optax.sgd(learning_rate=optax.cosine_decay_schedule(lr, epochs * len(train_loader), alpha), momentum=momentum),
+            optax.sgd(learning_rate=optax.cosine_decay_schedule(lr, epochs * len(train_loader), alpha) if epochs else lr, momentum=momentum),
         )
     else:
         raise NotImplementedError
@@ -112,7 +116,7 @@ def main(seed=42, log_dir=None, data_dir=None,
         optimizer = optax.chain(
             optax.add_decayed_weights(reg_scale),
             optax.sgd(learning_rate=swa_lr, momentum=momentum),
-            swag(len(train_loader), min(swa_epochs, swag_rank)),
+            swag(len(train_loader), swag_rank),
         )
     else:
         raise NotImplementedError
@@ -126,7 +130,7 @@ def main(seed=42, log_dir=None, data_dir=None,
     for e in tqdm(range(swa_epochs)):
         train_state = train_fn(train_state, train_loader, log_dir=log_dir, epoch=e)
 
-        if (e + 1) % 10 == 0:
+        if (e + 1) % 1 == 0:
             ## Update mutables like BatchNorm stats under SWA param.
             swa_train_state = TrainState.create(
                 apply_fn=model.apply,
@@ -156,7 +160,7 @@ def main(seed=42, log_dir=None, data_dir=None,
                 logging.debug(f'Constructing {swag_samples} samples for SWAG evaluation...')
 
                 swag_sample_params = jax.vmap(sample_swag, in_axes=(0, None, None))(
-                    jnp.array(samples_rng), swa_opt_state, min(swa_epochs, swag_rank))
+                    jnp.array(samples_rng), swa_opt_state, swag_rank)
             else:
                 logging.debug(f'Constructing {swag_samples} samples for SWAG-Diag evaluation...')
 

optax_swag/sample.py
@@ -9,8 +9,8 @@ from .state import SWAGDiagState, SWAGState
 
 
 @jax.jit
-def tree_split(key: chex.PRNGKey,
-               ref_tree: chex.ArrayTree) -> Tuple[chex.PRNGKey, chex.ArrayTree]:
+def tree_random_split(key: chex.PRNGKey,
+                      ref_tree: chex.ArrayTree) -> Tuple[chex.PRNGKey, chex.ArrayTree]:
 
     treedef = jax.tree_util.tree_structure(ref_tree)
 
@@ -20,9 +20,10 @@ def tree_split(key: chex.PRNGKey,
 
 
 @jax.jit
-def normal_tree(key: chex.PRNGKey, ref_tree: chex.ArrayTree) -> chex.ArrayTree:
+def tree_random_normal(key: chex.PRNGKey,
+                       ref_tree: chex.ArrayTree) -> chex.ArrayTree:
     
-    _, key_tree = tree_split(key, ref_tree)
+    _, key_tree = tree_random_split(key, ref_tree)
 
     return jax.tree_util.tree_map(lambda k, v: jax.random.normal(k, v.shape, v.dtype),
                                   key_tree, ref_tree)
@@ -34,24 +35,26 @@ def sample_swag_diag(key: chex.PRNGKey, state: SWAGDiagState, eps: float = 1e-30
     std_tree = jax.tree_util.tree_map(lambda mu, p2: jnp.sqrt(jnp.clip(p2 - jnp.square(mu), a_min=eps)),
                                       mean_tree, state.params2)
     
-    z_tree = normal_tree(key, mean_tree)
+    z_tree = tree_random_normal(key, mean_tree)
 
     return jax.tree_util.tree_map(lambda mu, std, z: mu + std * z, mean_tree, std_tree, z_tree)
 
 
 @partial(jax.jit, static_argnames=['rank'])
 def sample_swag(key: chex.PRNGKey, state: SWAGState, rank: int, scale: float = 1., eps: float = 1e-30) -> optax.Params:
-    mean_tree = state.mean
-    dparams_tree = state.dparams
-    std_tree = jax.tree_util.tree_map(lambda mu, p2: jnp.sqrt(jnp.clip(p2 - jnp.square(mu), a_min=eps)),
-                                      mean_tree, state.params2)
+    mean, unflatten_tree = jax.flatten_util.ravel_pytree(state.mean)
+    p2, _ = jax.flatten_util.ravel_pytree(state.params2)
+    
+    std = jnp.sqrt(jnp.clip(p2 - jnp.square(mean), a_min=eps))
+
+    dparams = jax.vmap(lambda tree: jax.flatten_util.ravel_pytree(tree)[0])(state.dparams)
 
     z1_key, z2_key = jax.random.split(key, 2)
-    z1_tree = normal_tree(z1_key, mean_tree)
-    z2 = jax.random.normal(z2_key, (rank,))
 
+    z1 = jax.random.normal(z1_key, mean.shape)
     z1_scale = scale / jnp.sqrt(2)
+
+    z2 = jax.random.normal(z2_key, (rank,))
     z2_scale = scale / jnp.sqrt(2 * (rank - 1))
 
-    return jax.tree_util.tree_map(lambda mu, std, dp, z1: mu + z1_scale * std * z1 + z2_scale * jnp.reshape(jnp.matmul(jnp.reshape(dp, (rank, -1)).T, z2), mu.shape),
-                                  mean_tree, std_tree, dparams_tree, z1_tree)
+    return unflatten_tree(mean + z1_scale * std * z1 + z2_scale * jnp.matmul(dparams.T, z2))

optax_swag/transform.py
@@ -20,8 +20,9 @@ def swa(freq: int) -> optax.GradientTransformation:
     update_mask = next_step == 0
     n = state.n + 1 * update_mask
 
-    next_params = jax.tree_util.tree_map(lambda u, p: u * update_mask + p, updates, params)
-    next_mean = jax.tree_util.tree_map(lambda mu, np: (n * mu + np) / (n + 1), 
+    next_params = jax.tree_util.tree_map(lambda p, u: jnp.where(update_mask, p + u, p),
+                                         params, updates)
+    next_mean = jax.tree_util.tree_map(lambda mu, np: jnp.where(update_mask, (n * mu + np) / (n + 1), mu),
                                        state.mean, next_params)
 
     return updates, SWAState(step=next_step, n=n, mean=next_mean)
@@ -44,10 +45,11 @@ def swag_diag(freq: int) -> optax.GradientTransformation:
     update_mask = next_step == 0
     n = state.n + 1 * update_mask
 
-    next_params = jax.tree_util.tree_map(lambda u, p: u * update_mask + p, updates, params)
-    next_mean = jax.tree_util.tree_map(lambda mu, np: (n * mu + np) / (n + 1), 
+    next_params = jax.tree_util.tree_map(lambda p, u: jnp.where(update_mask, p + u, p),
+                                         params, updates)
+    next_mean = jax.tree_util.tree_map(lambda mu, np: jnp.where(update_mask, (n * mu + np) / (n + 1), mu),
                                        state.mean, next_params)
-    next_params2 = jax.tree_util.tree_map(lambda v, np: (n * v + jnp.square(np)) / (n + 1),
+    next_params2 = jax.tree_util.tree_map(lambda p2, np: jnp.where(update_mask, (n * p2 + jnp.square(np)) / (n + 1), p2),
                                           state.params2, next_params)
 
     return updates, SWAGDiagState(step=next_step, n=n, mean=next_mean, params2=next_params2)
@@ -74,13 +76,14 @@ def swag(freq: int, rank: int) -> optax.GradientTransformation:
     update_mask = next_step == 0
     n = state.n + 1 * update_mask
 
-    next_params = jax.tree_util.tree_map(lambda u, p: u * update_mask + p, updates, params)
-    next_mean = jax.tree_util.tree_map(lambda mu, np: (n * mu + np) / (n + 1), 
+    next_params = jax.tree_util.tree_map(lambda p, u: jnp.where(update_mask, p + u, p),
+                                         params, updates)
+    next_mean = jax.tree_util.tree_map(lambda mu, np: jnp.where(update_mask, (n * mu + np) / (n + 1), mu),
                                        state.mean, next_params)
-    next_params2 = jax.tree_util.tree_map(lambda v, np: (n * v + jnp.square(np)) / (n + 1),
+    next_params2 = jax.tree_util.tree_map(lambda p2, np: jnp.where(update_mask, (n * p2 + jnp.square(np)) / (n + 1), p2),
                                           state.params2, next_params)
-    next_dparams = jax.tree_util.tree_map(lambda np, nmu, dp: jnp.where(update_mask, dp.at[state.c].set(np - nmu), dp),
-                                          next_params, next_mean, state.dparams)
+    next_dparams = jax.tree_util.tree_map(lambda dp, np, nmu: jnp.where(update_mask, dp.at[state.c].set(np - nmu), dp),
+                                          state.dparams, next_params, next_mean)
 
     c = (state.c + 1 * update_mask) % rank
 