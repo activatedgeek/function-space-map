10e1a628cf37da02a2f60f16710d65a41a1f0ceb
Sanyam Kapoor <sanyam@nyu.edu>
2023-03-19 13:08:45 -0400

    update fsvi results

--- notebooks/results/all_ctx_size.py
+++ notebooks/results/all_ctx_size.py
@@ -15,33 +15,31 @@ def get_summary_metrics(sweep_id, filter_func=None):
 
   return sweep, pd.DataFrame(data)
 
-cols = ['run_id', 'seed', 'context_points', 'context_transform', 'forward_points',
+cols = ['run_id', 'seed', 'context_set_size', 'context_subset',
         'nll_test', 'acc_sel_test', 'acc_test', 'ece_test', 'ood_auroc_entropy']
 
-_m1 = get_summary_metrics('deeplearn/fspace-inference/9xc5ptb1')[1][cols]
+_m1 = get_summary_metrics('deeplearn/fspace-inference/kmzrb0cz')[1][cols]
 _m1['dataset'] = 'CIFAR-10'
 
-_m2 = get_summary_metrics('deeplearn/fspace-inference/dblw56j7')[1][cols]
+_m2 = get_summary_metrics('deeplearn/fspace-inference/9wisuedm')[1][cols]
 _m2['dataset'] = 'FashionMNIST'
 
 metrics = pd.concat([_m1,_m2], ignore_index=True).rename(
     columns={ 'nll_test': 'nll', 'acc_sel_test': 'sel_acc', 'acc_test': 'acc', 'ece_test': 'ece', 'ood_auroc_entropy': 'ood_auroc'})
-# metrics.to_csv('results/all_ctx.csv', index=False)
+# metrics.to_csv('results/all_ctx_size.csv', index=False)
 
 cols = ['acc', 'sel_acc', 'nll', 'ece', 'ood_auroc']
 
-mu = metrics.groupby(['dataset', 'context_points', 'context_transform']).mean(numeric_only=True).drop(columns=['seed'])[cols]
+mu = metrics.groupby(['dataset', 'context_subset', 'context_set_size']).mean(numeric_only=True).drop(columns=['seed'])[cols]
 mu.acc = mu.acc.round(1)
 mu.sel_acc = mu.sel_acc.round(1)
 mu.nll = mu.nll.round(2)
 mu.ece = mu.ece.round(1)
 mu.ood_auroc = mu.ood_auroc.round(1)
 
-sigma = metrics.groupby(['dataset', 'context_points', 'context_transform']).std(numeric_only=True).drop(columns=['seed'])[cols]
+sigma = metrics.groupby(['dataset', 'context_subset', 'context_set_size']).std(numeric_only=True).drop(columns=['seed'])[cols]
 sigma.acc = sigma.acc.round(1)
 sigma.sel_acc = sigma.sel_acc.round(1)
 sigma.nll = sigma.nll.round(2)
 sigma.ece = sigma.ece.round(1)
 sigma.ood_auroc = sigma.ood_auroc.round(1)
-
-(mu.astype(str) + r' $\pm$ ' + sigma.astype(str))#.reset_index().to_markdown('tmp.md', index=False)
