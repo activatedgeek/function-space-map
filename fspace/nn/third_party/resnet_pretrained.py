fe6a1263981fd1e33d86b2b819739c4e7f3251d6
Sanyam Kapoor <sanyam@nyu.edu>
2023-10-18 16:14:48 -0400

    add resized cifar

---
+++ fspace/nn/third_party/resnet_pretrained.py
@@ -0,0 +1,16 @@
+import os
+import flaxmodels as fm
+
+from ..registry import register_model
+
+
+@register_model
+def resnet18_pt(model_dir=None, **_):
+    return fm.ResNet18(output='logits', pretrained='imagenet',
+                       ckpt_dir=model_dir or os.environ.get("MODELDIR"))
+
+
+@register_model
+def resnet50_pt(model_dir=None, **_):
+    return fm.ResNet50(output='logits', pretrained='imagenet',
+                       ckpt_dir=model_dir or os.environ.get("MODELDIR"))
